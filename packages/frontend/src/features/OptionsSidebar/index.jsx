1d7642a551f75bf1e7d8d1281847b9f0dbeb3113
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-17 14:10:12 +0000

    Import OptionButton into OptionsSidebar component

--- packages/frontend/src/features/OptionsSidebar/index.jsx
+++ packages/frontend/src/features/OptionsSidebar/index.jsx
@@ -1,12 +1,49 @@
+import PropTypes from "prop-types";
 import styles from "./index.module.css";
 
-const OptionsSidebar = () => {
+import OptionButton from "./components/OptionButton";
+
+const OptionsSidebar = ({
+    onOptionSelect,
+}) => {
     return (
         <div className={styles["wrapper"]}>
         <div className={styles["container"]}>
+            <div className={styles["chat-options"]}>
+                <OptionButton
+                    text="Friends"
+                    onClickHandler={() => onOptionSelect("friends")}
+                />
+                <OptionButton
+                    text="Groups"
+                    onClickHandler={() => onOptionSelect("groups")}
+                />
+                <OptionButton
+                    text="Communities"
+                    onClickHandler={() => onOptionSelect("communities")}
+                />
+            </div>
+            <div className={styles["settings-options"]}>
+                <OptionButton
+                    text="Account"
+                    onClickHandler={() => onOptionSelect("account")}
+                />
+                <OptionButton
+                    text="Settings"
+                    onClickHandler={() => onOptionSelect("settings")}
+                />
+                <OptionButton
+                    text="Sign Out"
+                    onClickHandler={() => onOptionSelect("sign-out")}
+                />
+            </div>
         </div>
         </div>
     );
 };
+
+OptionsSidebar.propTypes = {
+    onOptionSelect: PropTypes.func.isRequired,
+}
 
 export default OptionsSidebar;
