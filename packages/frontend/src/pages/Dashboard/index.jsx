1e035e7b85e45cbc434dca4097b8bdfed7d9ed55
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-17 11:59:02 +0000

    Create layout for main parts of Dashboard page component

--- packages/frontend/src/pages/Dashboard/index.jsx
+++ packages/frontend/src/pages/Dashboard/index.jsx
@@ -1,10 +1,22 @@
 import styles from "./index.module.css";
 
+import OptionsSidebar from "@/features/OptionsSidebar";
+import ChatSelectionPanel from "@/features/ChatSelectionPanel";
+import ChatPanel from "@/features/ChatPanel";
+
 const Dashboard = () => {
     return (
         <div className={styles["wrapper"]}>
         <div className={styles["container"]}>
-            <h1 className={styles["title"]} aria-label="title">Dashboard</h1>
+            <div className={styles["options-sidebar"]}>
+                <OptionsSidebar />
+            </div>
+            <div className={styles["chat-selection-panel"]}>
+                <ChatSelectionPanel />
+            </div>
+            <div className={styles["chat-panel"]}>
+                <ChatPanel />
+            </div>
         </div>
         </div>
     )
