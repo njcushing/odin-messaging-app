8ab5846fa7653d96760ef90a02972eb4a7df48f5
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-22 11:05:07 +0000

    Write tests for and add chat list options to ChatSelectionPanel

--- packages/frontend/src/features/ChatSelectionPanel/index.jsx
+++ packages/frontend/src/features/ChatSelectionPanel/index.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import styles from "./index.module.css";
 
+import ChatOption from "./components/ChatOption";
 import ChatPanel from "./components/ChatPanel";
 
 import * as getChatListFromAPI from "./utils/getChatListFromAPI.js";
@@ -35,14 +36,23 @@ const ChatSelectionPanel = ({
         <div className={styles["wrapper"]}>
         <div className={styles["container"]}>
             <ul className={styles["chat-list"]}>
+                <h3
+                    className={styles["chat-list-title"]}
+                    aria-label="chat-list-title"
+                >{chatType}</h3>
                 {chatList.map((chatOption, i) => {
                     return (
                         <li
-                            className={styles["chat-option"]}
+                            aria-label="chat-option"
                             key={i}
-                        >
-                            {chatOption.displayName}
-                        </li>
+                        ><ChatOption
+                            name={chatOption.name}
+                            tagLine={chatOption.tagLine}
+                            status={chatOption.status}
+                            imageSrc={chatOption.imageSrc}
+                            imageAlt={chatOption.imageAlt}
+                            onClickHandler={() => {}}
+                        /></li>
                     );
                 })}
             </ul>
