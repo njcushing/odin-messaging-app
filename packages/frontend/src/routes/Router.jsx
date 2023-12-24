e2411b7c3e327782e4d048ef3b982b5ab85dd3ee
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-24 13:18:40 +0000

    Add /create-account route to Router component

--- packages/frontend/src/routes/Router.jsx
+++ packages/frontend/src/routes/Router.jsx
@@ -1,14 +1,24 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import LogIn from "@/pages/LogIn";
+import CreateAccount from "@/pages/CreateAccount";
 import Dashboard from "@/pages/Dashboard";
 import ErrorPage from "@/pages/ErrorPage";
 
 const Router = () => {
     const browserRouter = createBrowserRouter([
         {
+            path: "/",
+            errorElement: <ErrorPage />,
+        },
+        {
             path: "/log-in",
             element: <LogIn />,
+            errorElement: <ErrorPage />,
+        },
+        {
+            path: "/create-account",
+            element: <CreateAccount />,
             errorElement: <ErrorPage />,
         },
         {
