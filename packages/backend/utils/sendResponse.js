8588f1fcd8fd04b4e636a05ca0c427e306aad5d9
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-30 14:04:00 +0000

    Fix bad responses being sent on invalid err argument in development mode

--- packages/backend/utils/sendResponse.js
+++ packages/backend/utils/sendResponse.js
@@ -6,9 +6,25 @@ const sendResponse = (
     err = null
 ) => {
     if (process.env.NODE_ENV === "development") {
+        let statusValue = 500;
+        if (Number.isInteger(status)) {
+            statusValue = status;
+        } else if (
+            err &&
+            typeof err.status !== undefined &&
+            Number.isInteger(err.status)
+        ) {
+            statusValue = err.status;
+        }
+
+        let stackString = "";
+        if (err && typeof err.stack === "string") {
+            stackString = err.stack;
+        }
+
         return res.status(status).send({
-            status: status || err.status,
-            stack: err.stack || "",
+            status: statusValue,
+            stack: stackString,
             message: message,
             data: null,
         });
