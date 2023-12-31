0eb1acee05ff366179d8309b1389bfa69e5761fa
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-31 12:53:31 +0000

    Update test database to generate users on creation

--- packages/backend/utils/dbConfigTesting.js
+++ packages/backend/utils/dbConfigTesting.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 
+import User from "../models/user.js";
+
 const initialiseMongoServer = async () => {
     // Create database and connect
     const mongoServer = await MongoMemoryServer.create();
@@ -20,6 +22,32 @@ const initialiseMongoServer = async () => {
     mongoose.connection.once("open", () => {
         console.log(`MongoDB successfully connected to ${mongoUri}`);
     });
+
+    // Populate database
+    const users = [];
+    await createUsers();
+
+    async function newUser(username, email, password) {
+        const user = new User({
+            username: username,
+            email: email,
+            password: password,
+        });
+        await user.save();
+        users[users.length] = user;
+    }
+
+    async function createUsers() {
+        await Promise.all([
+            newUser("Person1", "person1@company.com", "person1*"),
+            newUser("Person2", "person2@company.com", "person2*"),
+            newUser("Person3", "person3@company.com", "person3*"),
+            newUser("Person4", "person4@company.com", "person4*"),
+            newUser("Person5", "person5@company.com", "person5*"),
+        ]);
+    }
+
+    return [users];
 };
 
 export default initialiseMongoServer;
