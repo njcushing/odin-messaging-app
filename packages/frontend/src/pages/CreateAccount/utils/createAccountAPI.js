fe61af54cc7ddad91b4de62610ccf915f5b4cf30
njcushing <129661299+njcushing@users.noreply.github.com>
2023-12-31 11:42:49 +0000

    Write proper functionality for API function for creating accounts

--- packages/frontend/src/pages/CreateAccount/utils/createAccountAPI.js
+++ packages/frontend/src/pages/CreateAccount/utils/createAccountAPI.js
@@ -1,8 +1,39 @@
-const createAccountAPI = async () => {
-    return {
-        status: 400,
-        message: "Account creation attempt failed",
-    };
+const createAccountAPI = async (credentials) => {
+    const data = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/create-account`,
+        {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(credentials),
+        }
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            if (
+                responseJSON.data !== null &&
+                typeof responseJSON === "object" &&
+                responseJSON.hasOwnProperty("token")
+            ) {
+                localStorage.setItem(
+                    "odin-messaging-app-auth-token",
+                    responseJSON.data.token
+                );
+            }
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: 500,
+                message: "Account creation attempt failed",
+            };
+        });
+    return data;
 };
 
 export default createAccountAPI;
