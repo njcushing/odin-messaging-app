4a50d049e5479f9c126006cfadae97db446b6c76
njcushing <129661299+njcushing@users.noreply.github.com>
2024-01-04 10:55:50 +0000

    Write tests and functionality for /user/friend/can-be-added/:username GET route & update router

--- packages/backend/routes/user.js
+++ packages/backend/routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 import * as controller from "../controllers/userController.js";
 
 router.get("/self/:username", controller.userSelf);
+router.get("/friend/can-be-added/:username", controller.friendCanBeAdded);
 router.get("/friend/:username", controller.friendGet);
 router.post("/friends/:username", controller.friendsPost);
 router.get("/friends", controller.friendsGet);
