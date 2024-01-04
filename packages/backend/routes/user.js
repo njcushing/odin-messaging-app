1cb3b880b4d44bede87852d7451a3552ca34ed18
njcushing <129661299+njcushing@users.noreply.github.com>
2024-01-04 10:36:02 +0000

    Write tests and functionality for /user/self/:username route & update router

--- packages/backend/routes/user.js
+++ packages/backend/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 import * as controller from "../controllers/userController.js";
 
+router.get("/self/:username", controller.userSelf);
 router.get("/friend/:username", controller.friendGet);
 router.post("/friends/:username", controller.friendsPost);
 router.get("/friends", controller.friendsGet);
