4718c01e32dc2ee105ae25e777e847f2035efe4c
njcushing <129661299+njcushing@users.noreply.github.com>
2024-01-05 11:36:09 +0000

    Delete /dashboard routes

--- packages/backend/routes/index.js
+++ packages/backend/routes/index.js
@@ -3,10 +3,9 @@ const router = express.Router();
 
 import login from "./login.js";
 import user from "./user.js";
-import dashboard from "./dashboard.js";
 
 router.get("/", (req, res) => {
     res.send("");
 });
 
-export { router as index, login, user, dashboard };
+export { router as index, login, user };
