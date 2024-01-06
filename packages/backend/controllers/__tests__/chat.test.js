a9e894754366cbe34b453d227cd921fb3c1bdef2
njcushing <129661299+njcushing@users.noreply.github.com>
2024-01-06 12:00:46 +0000

    Write tests & functionality for /chat POST route & update router

--- packages/backend/controllers/__tests__/chat.test.js
+++ packages/backend/controllers/__tests__/chat.test.js
@@ -6,6 +6,7 @@ import jwt from "jsonwebtoken";
 import { vi } from "vitest";
 import chatRouter from "../../routes/chat.js";
 
+import User from "../../models/user.js";
 import Chat from "../../models/chat.js";
 
 import mongoose from "mongoose";
@@ -14,6 +15,17 @@ import initialiseMongoServer from "../../utils/dbConfigTesting.js";
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", chatRouter);
+
+const mockProtectedRouteJWT = (_id, username, password) => {
+    protectedRouteJWT.mockImplementationOnce((req, res, next) => {
+        req.user = {
+            _id: _id,
+            username: username,
+            password: password,
+        };
+        return next();
+    });
+};
 
 const protectedRouteJWT = vi.fn((req, res, next) => {
     req.user = {
@@ -45,6 +57,112 @@ describe("Route testing...", () => {
     describe("/chat/:chatId GET route...", () => {
         test(`Should respond with status code 200 on successful request`, async () => {
             await request(app).get(`/thisChatExists`).expect(200);
+        });
+    });
+
+    describe("/chat POST route...", () => {
+        test(`Should respond with status code 400 if the body object in the
+         request object does not contain the necessary information`, async () => {
+            await request(app)
+                .post(`/`)
+                .send()
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect(400);
+            await request(app)
+                .post(`/`)
+                .send({ participants: "" })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect(400);
+            await request(app)
+                .post(`/`)
+                .send({ participants: [] })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect(400);
+            await request(app)
+                .post(`/`)
+                .send({ participants: [null] })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect(400);
+        });
+        test(`Should respond with status code 400 if the user '_id' value
+         extracted from the token in the 'authorization' header is not a valid
+         MongoDB ObjectId`, async () => {
+            mockProtectedRouteJWT(null, "Person1", "person1*");
+            await request(app)
+                .post(`/`)
+                .send({ participants: [users[1]._id] })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect(400);
+        });
+        test(`Should respond with status code 404 if the currently-logged in
+         user is not found in the database`, async () => {
+            mockProtectedRouteJWT(
+                new mongoose.Types.ObjectId(),
+                "Person1",
+                "person1*"
+            );
+            await request(app)
+                .post(`/`)
+                .send({
+                    participants: [users[1]._id],
+                })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect(404);
+        });
+        test(`Should respond with status code 400 if at least one of the other
+         participants' _ids does not correspond to a valid user in the database`, async () => {
+            mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
+            await request(app)
+                .post(`/`)
+                .send({
+                    participants: [users[1]._id, new mongoose.Types.ObjectId()],
+                })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect(400);
+        });
+        test(`Should respond with status code 404 if at least one of the other
+         participants' _ids is not in the currently logged-in user's friends
+         list`, async () => {
+            mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
+            await request(app)
+                .post(`/`)
+                .send({ participants: [users[2]._id] })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect(404);
+        });
+        test(`Should respond with status code 500 if the new chat failed to save
+         after creation`, async () => {
+            vi.spyOn(Chat.prototype, "save").mockImplementationOnce(() =>
+                Promise.reject("fail update")
+            );
+            mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
+            await request(app)
+                .post(`/`)
+                .send({ participants: [users[1]._id] })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect(500);
+        });
+        test(`Should respond with status code 401 if the currently logged-in
+         user cannot be found in the database when attempting to update its
+         fields`, async () => {
+            mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
+            // generateToken.mockReturnValueOnce("Bearer token");
+            vi.spyOn(User, "findByIdAndUpdate").mockReturnValueOnce(null);
+            await request(app)
+                .post(`/`)
+                .send({ participants: [users[1]._id] })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect(401);
         });
     });
 });
--- packages/backend/controllers/chatController.js
+++ packages/backend/controllers/chatController.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 import asyncHandler from "express-async-handler";
 import { body, param } from "express-validator";
-import passport from "passport";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
 
+import User from "../models/user.js";
 import Chat from "../models/chat.js";
 
 import sendResponse from "../utils/sendResponse.js";
@@ -12,9 +10,139 @@ import checkRequestValidationError from "../utils/checkRequestValidationError.js
 import protectedRouteJWT from "../utils/protectedRouteJWT.js";
 import generateToken from "../utils/generateToken.js";
 
+const validateUserId = (res, next, userId) => {
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return next(sendResponse(res, 400, "User credentials are invalid."));
+    }
+};
+
+const userNotFound = (res, userId) => {
+    return sendResponse(res, 404, "User not found in database.");
+};
+
+const validators = {
+    participants: body("participants").custom((value, { req, loc, path }) => {
+        if (!Array.isArray(value)) {
+            throw new Error(
+                `'participants' field must be of type Array, got 
+                ${typeof participants}`
+            );
+        }
+        if (value.length === 0) {
+            throw new Error(`'participants' field (Array) must not be empty`);
+        }
+        for (let i = 0; i < value.length; i++) {
+            if (!mongoose.Types.ObjectId.isValid(value[i])) {
+                throw new Error(
+                    `'participants' field (Array) must only contain valid
+                    MongoDB ObjectId's, got ${value[i]} at index ${i}`
+                );
+            }
+        }
+        return value;
+    }),
+};
+
 export const chatGet = [
-    protectedRouteJWT,
     asyncHandler(async (req, res, next) => {
         sendResponse(res, 200, "Chat found.", { chat: {} });
+    }),
+];
+
+export const chatPost = [
+    protectedRouteJWT,
+    validators.participants,
+    checkRequestValidationError,
+    asyncHandler(async (req, res, next) => {
+        validateUserId(res, next, req.user._id);
+        let user = await User.findById(req.user._id).exec();
+        if (user === null) return userNotFound(res, next, req.user._id);
+
+        const token = await generateToken(req.user.username, req.user.password);
+
+        // Validate that all participants are actual users within database
+        let invalidParticipant = false;
+        await Promise.all(
+            req.body.participants.map((participant) => {
+                return new Promise(async (resolve, reject) => {
+                    const user = await User.findById(participant).exec();
+                    if (user === null)
+                        reject(
+                            new Error(
+                                `User not found in database: ${participant}`
+                            )
+                        );
+                    resolve(user);
+                });
+            })
+        ).catch((err) => {
+            invalidParticipant = true;
+        });
+        if (invalidParticipant) {
+            return sendResponse(
+                res,
+                400,
+                "Not all participants could be found in the database.",
+                { token: token }
+            );
+        }
+
+        // Ensure all participants are on currently logged-in user's friends list
+        for (let i = 0; i < req.body.participants.length; i++) {
+            if (!user.friends.includes(req.body.participants[i].toString())) {
+                return sendResponse(
+                    res,
+                    404,
+                    `${req.body.participants[i]} is not on the currently
+                    logged-in user's friends list`,
+                    { token: token }
+                );
+            }
+        }
+
+        // Create and save new chat
+        const chat = new Chat({
+            participants: req.body.participants.map((participant) => {
+                return { user: participant };
+            }),
+        });
+        chat.participants.push({
+            user: user._id,
+            role: "admin", // On chat creation, only creator is admin
+        });
+        await chat
+            .save()
+            .then(async (chat) => {
+                let updatedUser = await User.findByIdAndUpdate(user._id, {
+                    $push: { chats: chat._id },
+                });
+                if (updatedUser === null) {
+                    await Chat.findByIdAndDelete(chat._id);
+                    return sendResponse(
+                        res,
+                        401, // Signalling client to re-direct to /log-in instead of sending 404
+                        `User not found in database: ${user._id}.`,
+                        { token: token }
+                    );
+                }
+                sendResponse(
+                    res,
+                    201,
+                    `Chat successfully created at: ${chat._id}.`,
+                    {
+                        chatId: chat._id,
+                        token: token,
+                    }
+                );
+            })
+            .catch((err) => {
+                sendResponse(
+                    res,
+                    500,
+                    "Chat could not be created.",
+                    { token: token },
+                    err
+                );
+            });
     }),
 ];
