e6f312c20d9bdfa031d0e0e194613edce3fb0940
njcushing <129661299+njcushing@users.noreply.github.com>
2024-01-13 11:40:30 +0000

    Set 'getters' option to true on User model

--- packages/backend/models/user.js
+++ packages/backend/models/user.js
@@ -8,83 +8,92 @@ import {
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    username: {
-        type: String,
-        trim: true,
-        unique: true,
-        validate: {
-            validator: function (value) {
-                return validateUsername(value).status;
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            unique: true,
+            validate: {
+                validator: function (value) {
+                    return validateUsername(value).status;
+                },
+                message: (props) => validateUsername(props.value).message.back,
             },
-            message: (props) => validateUsername(props.value).message.back,
+            required: [true, "'username' field required"],
         },
-        required: [true, "'username' field required"],
-    },
-    email: {
-        type: String,
-        trim: true,
-        unique: true,
-        validate: {
-            validator: function (value) {
-                return validateEmail(value).status;
+        email: {
+            type: String,
+            trim: true,
+            unique: true,
+            validate: {
+                validator: function (value) {
+                    return validateEmail(value).status;
+                },
+                message: (props) => validateEmail(props.value).message.back,
             },
-            message: (props) => validateEmail(props.value).message.back,
+            required: [true, "'email' field required"],
         },
-        required: [true, "'email' field required"],
-    },
-    password: {
-        type: String,
-        trim: true,
-        required: [true, "'password' field required"],
-    },
-    admin: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
-    account_creation_date: { type: Date, default: Date.now },
-    friends: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: "User",
-                required: true,
+        password: {
+            type: String,
+            trim: true,
+            required: [true, "'password' field required"],
+        },
+        admin: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        account_creation_date: { type: Date, default: Date.now },
+        friends: [
+            {
+                user: {
+                    type: Schema.Types.ObjectId,
+                    ref: "User",
+                    required: true,
+                },
+                chat: {
+                    type: Schema.Types.ObjectId,
+                    ref: "Chat",
+                    default: null,
+                },
+                becameFriendsDate: { type: Date, default: Date.now },
+                friendStatus: {
+                    type: String,
+                    enum: ["normal", "unfriended"],
+                    default: "normal",
+                },
             },
-            chat: { type: Schema.Types.ObjectId, ref: "Chat", default: null },
-            becameFriendsDate: { type: Date, default: Date.now },
-            friendStatus: {
+        ],
+        friendRequests: [{ type: Schema.Types.ObjectId, ref: "User" }],
+        chats: [{ type: Schema.Types.ObjectId, ref: "Chat" }],
+        communities: [{ type: Schema.Types.ObjectId, ref: "Community" }],
+        lastActivity: { type: Date, default: Date.now },
+        preferences: {
+            displayName: {
                 type: String,
-                enum: ["normal", "unfriended"],
-                default: "normal",
+                trim: true,
+                maxlength: 40,
+                default: "",
+            },
+            tagLine: {
+                type: String,
+                trim: true,
+                maxlength: 100,
+                default: "",
+            },
+            image: { type: Schema.Types.ObjectId, ref: "Image" },
+            setStatus: {
+                type: String,
+                enum: ["online", "busy", "away", "offline", null],
+                default: null,
             },
         },
-    ],
-    friendRequests: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    chats: [{ type: Schema.Types.ObjectId, ref: "Chat" }],
-    communities: [{ type: Schema.Types.ObjectId, ref: "Community" }],
-    lastActivity: { type: Date, default: Date.now },
-    preferences: {
-        displayName: {
-            type: String,
-            trim: true,
-            maxlength: 40,
-            default: "",
-        },
-        tagLine: {
-            type: String,
-            trim: true,
-            maxlength: 100,
-            default: "",
-        },
-        image: { type: Schema.Types.ObjectId, ref: "Image" },
-        setStatus: {
-            type: String,
-            enum: ["online", "busy", "away", "offline", null],
-            default: null,
-        },
     },
-});
+    {
+        getters: true,
+    }
+);
 
 UserSchema.virtual("status").get(function () {
     if (this.preferences.setStatus !== null) return this.preferences.setStatus;
