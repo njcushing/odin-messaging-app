23e993dd8e480239559baef82e039f62f1bcb0e7
njcushing <129661299+njcushing@users.noreply.github.com>
2024-01-14 11:15:40 +0000

    Write new API function for requesting currently logged-in user data

---
+++ packages/frontend/src/features/ChatsPanel/components/ChatPanel/utils/getSelf.js
@@ -0,0 +1,49 @@
+const getSelf = async (abortController) => {
+    const data = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/user/self`,
+        {
+            signal: abortController ? abortController.signal : null,
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: localStorage.getItem(
+                    "odin-messaging-app-auth-token"
+                ),
+            },
+        }
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromResponseJSON(responseJSON);
+
+            if (responseJSON.status === 401) {
+                window.location.href = "/log-in";
+            }
+
+            let user = null;
+            if (
+                responseJSON.data !== null &&
+                typeof responseJSON.data === "object" &&
+                "user" in responseJSON.data
+            ) {
+                user = responseJSON.data.user;
+            }
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                user: user,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: 500,
+                message: "Getting currently logged-in user's data failed",
+                user: null,
+            };
+        });
+    return data;
+};
+
+export default getSelf;
