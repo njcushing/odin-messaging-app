ae60ee63486027be968b6fa180ff1f316cd40206
njcushing <129661299+njcushing@users.noreply.github.com>
2024-01-19 12:32:00 +0000

    Write API functions for updating user's display name, tag line & status

--- utils/updateUserFields.js
+++ utils/updateUserFields.js
@@ -1,22 +1,121 @@
-export const updateDisplayName = (displayName, abortController) => {
-    return {
-        status: 200,
-        message: "Done.",
-    };
+export const updateDisplayName = async (displayName, abortController) => {
+    const data = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/user/preferences/displayName`,
+        {
+            signal: abortController ? abortController.signal : null,
+            method: "PUT",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: localStorage.getItem(
+                    "odin-messaging-app-auth-token"
+                ),
+            },
+            body: JSON.stringify({
+                displayName: displayName,
+            }),
+        }
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromResponseJSON(responseJSON);
+
+            if (responseJSON.status === 401) {
+                window.location.href = "/log-in";
+            }
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: 500,
+                message: "Updating display name failed.",
+            };
+        });
+    return data;
 };
 
-export const updateTagLine = (tagLine, abortController) => {
-    return {
-        status: 200,
-        message: "Done.",
-    };
+export const updateTagLine = async (tagLine, abortController) => {
+    const data = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/user/preferences/tagLine`,
+        {
+            signal: abortController ? abortController.signal : null,
+            method: "PUT",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: localStorage.getItem(
+                    "odin-messaging-app-auth-token"
+                ),
+            },
+            body: JSON.stringify({
+                tagLine: tagLine,
+            }),
+        }
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromResponseJSON(responseJSON);
+
+            if (responseJSON.status === 401) {
+                window.location.href = "/log-in";
+            }
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: 500,
+                message: "Updating tag line failed.",
+            };
+        });
+    return data;
 };
 
-export const updateStatus = (status, abortController) => {
-    return {
-        status: 200,
-        message: "Done.",
-    };
+export const updateStatus = async (status, abortController) => {
+    const data = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/user/preferences/setStatus`,
+        {
+            signal: abortController ? abortController.signal : null,
+            method: "PUT",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: localStorage.getItem(
+                    "odin-messaging-app-auth-token"
+                ),
+            },
+            body: JSON.stringify({
+                setStatus: status,
+            }),
+        }
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromResponseJSON(responseJSON);
+
+            if (responseJSON.status === 401) {
+                window.location.href = "/log-in";
+            }
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: 500,
+                message: "Updating tag line failed.",
+            };
+        });
+    return data;
 };
 
 export const updateProfileImage = (profileImage, abortController) => {
