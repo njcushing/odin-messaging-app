7125a24e7a3b03ef8ff04a5f8d299afbf65a5a05
njcushing <129661299+njcushing@users.noreply.github.com>
2024-01-21 12:12:12 +0000

    Write tests and functionality for new Settings feature component

---
+++ packages/frontend/src/features/Settings/index.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect, useCallback, useRef } from "react"
+import PropTypes from "prop-types";
+import styles from "./index.module.css";
+
+import FieldUpdater from "@/components/FieldUpdater";
+
+import * as validateUserFields from '../../../../../utils/validateUserFields.js'
+import * as updateUserFields from '../../utils/updateUserFields.js'
+
+const Settings = ({
+    userSettings,
+    onUpdateHandler,
+}) => {
+    return (
+        <div className={styles["wrapper"]}>
+        <div className={styles["container"]}>
+            <div className={styles["settings"]}>
+                <h1
+                    className={styles["title"]}
+                    aria-label="settings-panel"
+                >Settings</h1>
+                <div className={styles["settings-container"]}>
+                    <FieldUpdater
+                        labelText="Theme"
+                        fieldName="theme"
+                        initialValue={userSettings.theme}
+                        validator={validateUserFields.theme}
+                        apiFunction={updateUserFields.theme}
+                        context={{
+                            type: "circles",
+                            options: validateUserFields.themeOptions().map((theme) => {
+                                return {
+                                    name: theme.name,
+                                    colour: theme.colour,
+                                    tooltipText: theme.name,
+                                    tooltipPosition: "bottom",
+                                }
+                            }),
+                            widthPx: 64,
+                            heightPx: 64,
+                        }}
+                        onUpdateHandler={() => onUpdateHandler()}
+                    />
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+Settings.propTypes = {
+    userSettings: PropTypes.shape({
+        theme: PropTypes.oneOf(["default", "red", "orange", "green", "blue", "purple", "light", "dark"]),
+    }),
+    onUpdateHandler: PropTypes.func,
+}
+
+Settings.defaultProps = {
+    userSettings: {
+        theme: "default",
+    },
+    onUpdateHandler: () => {},
+}
+
+export default Settings;
