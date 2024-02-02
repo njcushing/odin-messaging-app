cddacf9be8cd31530b785539fe0b04045f286059
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-02 11:42:46 +0000

    Update test suite for Tooltip component

--- packages/frontend/src/components/Tooltip/Tooltip.test.jsx
+++ packages/frontend/src/components/Tooltip/Tooltip.test.jsx
@@ -7,15 +7,60 @@ import '@testing-library/jest-dom'
 import { BrowserRouter } from "react-router-dom";
 import Tooltip from './index.jsx'
 
-const renderComponent = () => {
-    render(<Tooltip
-        text="Tooltip"
-        position="bottom"
-        pixelBuffer={6}
+const renderComponent = (
+    text = "Tooltip",
+    position = "bottom",
+    pixelBuffer = 6,
+) => {
+    return render(<Tooltip
+        text={text}
+        position={position}
+        pixelBuffer={pixelBuffer}
     />);
 }
 
 describe("UI/DOM Testing...", () => {
+    describe("The element...", () => {
+        test(`Should return a 'null' element if the provided 'text' prop's value
+         has a length of 0 characters, otherwise it should be present in the
+         document`, () => {
+            let tooltip;
+            
+            renderComponent("");
+            tooltip = screen.queryByRole("generic", { name: "tooltip" });
+            expect(tooltip).toBeNull();
+
+            renderComponent();
+            tooltip = screen.getByRole("generic", { name: "tooltip" });
+            expect(tooltip).not.toBeNull();
+        });
+        test(`Should throw no errors on mount regardless of which of the four
+         values ("top", "right", "bottom", "left") is used in the 'position'
+         prop`, () => {
+            const consoleErrorSpy = vi.spyOn(console, "error");
+            
+            renderComponent("Tooltip", "top");
+            expect(consoleErrorSpy).not.toHaveBeenCalled();
+            consoleErrorSpy.mockClear();
+            
+            renderComponent("Tooltip", "right");
+            expect(consoleErrorSpy).not.toHaveBeenCalled();
+            consoleErrorSpy.mockClear();
+            
+            renderComponent("Tooltip", "bottom");
+            expect(consoleErrorSpy).not.toHaveBeenCalled();
+            consoleErrorSpy.mockClear();
+            
+            renderComponent("Tooltip", "left");
+            expect(consoleErrorSpy).not.toHaveBeenCalled();
+            consoleErrorSpy.mockClear();
+            
+            consoleErrorSpy.mockReturnValueOnce(); // suppress console error
+            renderComponent("Tooltip", "other");
+            expect(consoleErrorSpy).toHaveBeenCalled();
+            consoleErrorSpy.mockClear();
+        });
+    });
     describe("The text element...", () => {
         test(`Should have the same textContent as the provided 'text' prop's
          value`, () => {
