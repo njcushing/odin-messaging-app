cb01a173a76f173e152e3c2b059f53ce7fe40894
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-02 11:22:18 +0000

    Update test suite for MessageBox component

--- packages/frontend/src/features/ChatsPanel/components/ChatPanel/components/MessageBox/MessageBox.test.jsx
+++ packages/frontend/src/features/ChatsPanel/components/ChatPanel/components/MessageBox/MessageBox.test.jsx
@@ -1,11 +1,13 @@
 /* global describe, test, expect */
 
 import { vi } from 'vitest'
-import {render, screen} from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import { BrowserRouter } from "react-router-dom"
 import MessageBox from './index.jsx'
+
+import * as validateMessage from "../../../../../../../../../utils/validateMessageFields.js";
 
 const renderComponent = (
     text = "Sample Text",
@@ -20,6 +22,18 @@ const renderComponent = (
         onSubmitHandler={onSubmitHandler}
     />);
 }
+
+const validateImage = vi.fn(() => ({
+    status: true,
+    message: "Valid image",
+}));
+vi.mock('../../../../../../../../../utils/validateMessageFields.js', async () => {
+    const actual = await vi.importActual("../../../../../../../../../utils/validateMessageFields.js");
+    return {
+        ...actual,
+        image: () => validateImage(),
+    }
+});
 
 describe("UI/DOM Testing...", () => {
     describe("The textarea element...", () => {
@@ -38,7 +52,86 @@ describe("UI/DOM Testing...", () => {
             expect(textbox.textContent).toBe("Sample Text");
         });
     });
-    describe("The form-submission button...", () => {
+    describe("The 'Upload' button...", () => {
+        test(`When a file is selected after clicking it, if an error is thrown,
+         the 'validateMessageFields.image' should not be invoked`, async () => {
+            const validateImageSpy = vi.spyOn(validateMessage, "image");
+
+            const user = userEvent.setup();
+            renderComponent();
+
+            const uploadButton = screen.getByLabelText("upload-image");
+            
+            const file = new File(['hello'], 'hello.png', null);
+            await user.upload(uploadButton, file);
+
+            expect(validateImageSpy).not.toHaveBeenCalled();
+        });
+        test(`When a file is selected after clicking it, the
+         'validateMessageFields.image' should be invoked`, async () => {
+            const validateImageSpy = vi.spyOn(validateMessage, "image");
+
+            const user = userEvent.setup();
+            
+            renderComponent();
+
+            const uploadButton = screen.getByLabelText("upload-image");
+            
+            const file = new File(['hello'], 'hello.png', {type: 'image/png'});
+            await user.upload(uploadButton, file);
+
+            expect(validateImageSpy).toHaveBeenCalled();
+        });
+        test(`When a file is selected after clicking it, if the file passes the
+         validation, the onSubmitHandler callback function should be invoked`, async () => {
+            validateImage.mockReturnValueOnce({
+                status: true,
+                message: "Valid image",
+            });
+
+            const user = userEvent.setup();
+            const callback = vi.fn();
+            
+            renderComponent(
+                "Sample Text",
+                "Write something",
+                [],
+                callback,
+            );
+
+            const uploadButton = screen.getByLabelText("upload-image");
+            
+            const file = new File(['hello'], 'hello.png', {type: 'image/png'});
+            await user.upload(uploadButton, file);
+
+            expect(callback).toHaveBeenCalled();
+        });
+        test(`When a file is selected after clicking it, if the file fails the
+         validation, the onSubmitHandler callback function should not be invoked`, async () => {
+            validateImage.mockReturnValueOnce({
+                status: false,
+                message: "Invalid image",
+            });
+
+            const user = userEvent.setup();
+            const callback = vi.fn();
+            
+            renderComponent(
+                "Sample Text",
+                "Write something",
+                [],
+                callback,
+            );
+
+            const uploadButton = screen.getByLabelText("upload-image");
+            
+            const file = new File(['hello'], 'hello.png', {type: 'image/png'});
+            await user.upload(uploadButton, file);
+
+            expect(callback).not.toHaveBeenCalled();
+        });
+    });
+    describe("The 'Send' button...", () => {
         test(`When clicked, should invoke the provided callback function`, async () => {
             const user = userEvent.setup();
             const callback = vi.fn();
@@ -50,7 +143,8 @@ describe("UI/DOM Testing...", () => {
                 callback,
             );
             const submitButton = screen.getByRole("button", { name: "send-message-button" });
-
+            
+            fireEvent.mouseLeave(submitButton);
             await user.click(submitButton);
 
             expect(callback).toHaveBeenCalled();
