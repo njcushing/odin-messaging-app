272471e99b8674f582751e5b4eff1e3db74c1b26
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-02 12:14:44 +0000

    Update test suite for Message component

--- packages/frontend/src/features/ChatsPanel/components/ChatPanel/components/Message/Message.test.jsx
+++ packages/frontend/src/features/ChatsPanel/components/ChatPanel/components/Message/Message.test.jsx
@@ -7,35 +7,41 @@ import '@testing-library/jest-dom'
 import { BrowserRouter } from "react-router-dom"
 import Message from './index.jsx'
 
-const renderComponent = (
-    text = "Sample Text",
-    image = {
+const props = {
+    text: "Sample Text",
+    image: {
         src: new Uint8Array(),
         alt: "body image alt text",
     },
-    name = "My Name",
-    dateSent = "2023-01-01T00:00:00",
-    profileImage = {
+    name: "My Name",
+    dateSent: "2023-01-01T00:00:00",
+    profileImage: {
         src: new Uint8Array(),
         alt: "body image alt text",
         status: null,
     },
-    position = "right",
-    replyingTo = {
+    position: "right",
+    replyingTo: {
         author: "Friend 1",
-        message: "Message text",
+        text: "Message text",
+        image: {
+            src: new Uint8Array(),
+            alt: "replying to image alt text",
+        }
     },
-    onReplyToHandler = () => {},
-) => {
+    onReplyToHandler: () => {},
+}
+
+const renderComponent = (props) => {
     render(<Message
-        text={text}
-        image={image}
-        name={name}
-        dateSent={dateSent}
-        profileImage={profileImage}
-        position={position}
-        replyingTo={replyingTo}
-        onReplyToHandler={onReplyToHandler}
+        text={props.text}
+        image={props.image}
+        name={props.name}
+        dateSent={props.dateSent}
+        profileImage={props.profileImage}
+        position={props.position}
+        replyingTo={props.replyingTo}
+        onReplyToHandler={props.onReplyToHandler}
     />);
 }
 
@@ -43,28 +49,117 @@ vi.mock('@/components/ProfileImage', () => ({
     default: ({
         src,
         alt,
+        status,
         sizePx,
     }) => {
-        return (<></>);
+        return (<div aria-label="profile-image"></div>);
     }
 }));
 
 global.URL.createObjectURL = vi.fn(() => 'image');
 
-describe("UI/DOM Testing...", () => {
-    describe("The <p> element displaying the message text...", () => {
-        test(`Should have the same textContent as the provided 'text' prop's
-         value`, () => {
-            renderComponent();
+describe(`Prop Testing...`, () => {
+    describe(`The 'image' prop...`, () => {
+        test(`When specified, should throw an error if the value of its property
+         'src' is not either a Typed Array or number (to allow 'null')`, () => {
+            const consoleErrorSpy = vi.spyOn(console, "error");
+            consoleErrorSpy.mockReturnValueOnce(); // suppress console error
+            renderComponent({ ...props, image: { src: "", alt: "" } });
+            expect(consoleErrorSpy).toHaveBeenCalled();
+
+            consoleErrorSpy.mockClear();
+
+            renderComponent(props);
+            expect(consoleErrorSpy).not.toHaveBeenCalled();
+        });
+    });
+    describe(`The 'replyingTo' prop...`, () => {
+        test(`When specified, should throw an error if the value of its property
+         'image.src' is not either a Typed Array or number (to allow 'null')`, () => {
+            const consoleErrorSpy = vi.spyOn(console, "error");
+            consoleErrorSpy.mockReturnValueOnce(); // suppress console error
+            renderComponent({ ...props, replyingTo: {
+                author: "",
+                text: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+            }});
+            expect(consoleErrorSpy).toHaveBeenCalled();
+
+            consoleErrorSpy.mockClear();
+
+            renderComponent(props);
+            expect(consoleErrorSpy).not.toHaveBeenCalled();
+        });
+    });
+});
+describe(`UI/DOM Testing...`, () => {
+    describe(`The <p> element displaying the message author name and date...`, () => {
+        test(`Should be present in the document`, () => {
+            renderComponent(props);
+            const nameAndDate = screen.getByText("My Name at 01/01/2023, 00:00:00");
+            expect(nameAndDate).toBeInTheDocument();
+        });
+    });
+    describe(`The user's profile image...`, () => {
+        test(`Should be present in the document`, () => {
+            renderComponent(props);
+            const profileImage = screen.getByRole("generic", { name: "profile-image" });
+            expect(profileImage).toBeInTheDocument();
+        });
+    });
+    describe(`The <p> element displaying the message text...`, () => {
+        test(`Should be present in the document if the 'text' prop has a value
+         of type String with a length greater than 0 characters`, () => {
+            renderComponent(props);
             const messageText = screen.getByText("Sample Text");
             expect(messageText).toBeInTheDocument();
         });
+        test(`Should not be present in the document if the 'text' prop has a
+         value of type String with a length of 0 characters`, () => {
+            renderComponent({ ...props, text: "" });
+            const messageText = screen.queryByText("Sample Text");
+            expect(messageText).toBeNull();
+        });
     });
-    describe("The <p> element displaying the message author name and date...", () => {
-        test(`Should be present in the document`, () => {
-            renderComponent();
-            const nameAndDate = screen.getByText("My Name at 01/01/2023, 00:00:00");
-            expect(nameAndDate).toBeInTheDocument();
+    describe(`The element displaying the message image...`, () => {
+        test(`Should be present in the document if the 'image' prop is an object
+         containing two properties: 'src' with a value of type Typed Array, and
+         'alt' with a value of type String`, () => {
+            renderComponent(props);
+            const messageImage = screen.getByRole("img", { name: "message-image" });
+            expect(messageImage).toBeInTheDocument();
+            expect(messageImage.alt).toBe(props.image.alt);
+        });
+        test(`Should not be present in the document if the 'image' prop has a
+         value of 'null'`, () => {
+            renderComponent({ ...props, image: null });
+            const messageImage = screen.queryByRole("img", { name: "message-image" });
+            expect(messageImage).toBeNull();
+        });
+    });
+    describe(`The element displaying the replying-to message image...`, () => {
+        test(`Should be present in the document if the 'replyingTo' prop is an
+         object containing a property: 'image' with a property: 'src' with a
+         value of type Typed Array, and 'alt' with a value of type String`, () => {
+            renderComponent(props);
+            const replyingToMessageImage = screen.getByRole("img", { name: "replying-to-message-image" });
+            expect(replyingToMessageImage).toBeInTheDocument();
+            expect(replyingToMessageImage.alt).toBe(props.replyingTo.image.alt);
+        });
+        test(`Otherwise, it should not be present in the document`, () => {
+            renderComponent({ ...props, replyingTo: {
+                author: "",
+                text: "",
+                image: {
+                    src: null,
+                    alt: "",
+                },
+            }});
+            const replyingToMessageImage = screen.queryByRole("img", { name: "replying-to-message-image" });
+            expect(replyingToMessageImage).toBeNull();
         });
     });
 });
