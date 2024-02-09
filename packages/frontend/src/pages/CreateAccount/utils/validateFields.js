a8570145443f6a82d41331108a2c6c93d472ea58
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:58 +0000

    Write email validation for CreateAccount component form

--- packages/frontend/src/pages/CreateAccount/utils/validateFields.js
+++ packages/frontend/src/pages/CreateAccount/utils/validateFields.js
@@ -1,4 +1,6 @@
 const usernamePattern = /^[a-zA-Z0-9]*$/;
+const emailPattern =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 const passwordPattern =
     /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
 
@@ -24,6 +26,25 @@ export const validateUsername = (username) => {
     return {
         status: true,
         message: "Valid Username.",
+    };
+};
+
+export const validateEmail = (email) => {
+    if (email.length === 0) {
+        return {
+            status: false,
+            message: "Your Email must not be empty.",
+        };
+    }
+    if (!email.match(emailPattern)) {
+        return {
+            status: false,
+            message: "Your Email must be in the format: name@company.com.",
+        };
+    }
+    return {
+        status: true,
+        message: "Valid Email.",
     };
 };
 
