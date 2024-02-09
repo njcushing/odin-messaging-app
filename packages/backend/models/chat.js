e95c0a6d7bc27dc09b743e6f3fe863a88efcc09f
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:36:00 +0000

    Remove 'lastActivity' field from User and Chat models; replace with timestamps option

--- packages/backend/models/chat.js
+++ packages/backend/models/chat.js
@@ -2,28 +2,34 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const ChatSchema = new Schema({
-    type: {
-        type: String,
-        enum: ["individual", "group"],
-        require: true,
-    },
-    participants: [
-        {
-            user: { type: Schema.Types.ObjectId, ref: "User", require: true },
-            nickname: { type: String, default: "" },
-            role: {
-                type: String,
-                enum: ["admin", "moderator", "guest"],
-                default: "guest",
-            },
-            muted: { type: Boolean, default: false },
+const ChatSchema = new Schema(
+    {
+        type: {
+            type: String,
+            enum: ["individual", "group"],
+            require: true,
         },
-    ],
-    name: { type: String, trim: true, default: "" },
-    image: { type: Schema.Types.ObjectId, ref: "Image" },
-    messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
-    lastActivity: { type: Date, default: Date.now },
-});
+        participants: [
+            {
+                user: {
+                    type: Schema.Types.ObjectId,
+                    ref: "User",
+                    require: true,
+                },
+                nickname: { type: String, default: "" },
+                role: {
+                    type: String,
+                    enum: ["admin", "moderator", "guest"],
+                    default: "guest",
+                },
+                muted: { type: Boolean, default: false },
+            },
+        ],
+        name: { type: String, trim: true, default: "" },
+        image: { type: Schema.Types.ObjectId, ref: "Image" },
+        messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+    },
+    { timestamps: true }
+);
 
 export default mongoose.model("Chat", ChatSchema);
