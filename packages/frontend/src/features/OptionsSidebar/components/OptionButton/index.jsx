65604a0ba56e3bc573c2320eea7025c780020bad
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:57 +0000

    Utilise new Tooltip component within OptionButton component

--- packages/frontend/src/features/OptionsSidebar/components/OptionButton/index.jsx
+++ packages/frontend/src/features/OptionsSidebar/components/OptionButton/index.jsx
@@ -1,11 +1,25 @@
+import { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import './index.css';
 
+import Tooltip from "@/components/Tooltip";
+import TooltipPropTypes from "@/components/Tooltip/TooltipPropTypes.jsx";
+
 const OptionButton = ({
     text,
+    tooltipText,
+    tooltipPosition,
     onClickHandler,
 }) => {
+    const [tooltipElement, setTooltipElement] = useState(null);
+
     return (
+        <div
+            className="container"
+            style={{
+                position: "relative",
+            }}
+        >
         <button
             className="option-button"
             aria-label={"option-button"}
@@ -14,22 +28,35 @@ const OptionButton = ({
                 e.currentTarget.blur();
                 e.preventDefault();
             }}
+            onMouseEnter={() => {
+                setTooltipElement(
+                    <Tooltip
+                        text={tooltipText}
+                        position={tooltipPosition}
+                    />
+                );
+            }}
             onMouseLeave={(e) => {
                 e.currentTarget.blur();
+                setTooltipElement(null);
             }}
         >
             <ul><li className="material-symbols-rounded">{text}</li></ul>
         </button>
+        {tooltipElement}
+        </div>
     )
 };
 
 OptionButton.propTypes = {
-    text: PropTypes.string,
+    text: PropTypes.string.isRequired,
+    tooltipText: PropTypes.string.isRequired,
+    tooltipPosition: TooltipPropTypes.position,
     onClickHandler: PropTypes.func.isRequired,
 }
 
-OptionButton.defaultProps = {
-    text: "Button",
+OptionButton.propTypes = {
+    tooltipPosition: "bottom",
 }
 
 export default OptionButton;
