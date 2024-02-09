e38f118a1ca2b8aab46b368d57d5830662a8834f
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:59 +0000

    Write functions for accepting and declining friend requests from client

---
+++ packages/frontend/src/features/ChatSelectionPanel/utils/declineFriendRequest.js
@@ -0,0 +1,48 @@
+const declineFriendRequest = async (username) => {
+    const data = await fetch(
+        `${
+            import.meta.env.VITE_SERVER_DOMAIN
+        }/user/friend-requests/${username}/decline`,
+        {
+            method: "PUT",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: localStorage.getItem(
+                    "odin-messaging-app-auth-token"
+                ),
+            },
+        }
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            if (responseJSON.status === 401) {
+                window.location.href = "/log-in";
+            }
+            if (
+                responseJSON.data !== null &&
+                typeof responseJSON.data === "object" &&
+                "token" in responseJSON.data
+            ) {
+                localStorage.setItem(
+                    "odin-messaging-app-auth-token",
+                    responseJSON.data.token
+                );
+            }
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                friendRequests: responseJSON.data.friendRequests,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: 500,
+                message: "Declining friend request failed",
+                friendRequests: [],
+            };
+        });
+    return data;
+};
+
+export default declineFriendRequest;
