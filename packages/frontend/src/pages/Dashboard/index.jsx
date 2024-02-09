40052cc4ce61a35af6fbb4f3a4d5532a9c2e9f09
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:57 +0000

    Write switch statement to decide Dashboard content type based on option selected

--- packages/frontend/src/pages/Dashboard/index.jsx
+++ packages/frontend/src/pages/Dashboard/index.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import styles from "./index.module.css";
 
 import OptionsSidebar from "@/features/OptionsSidebar";
@@ -5,18 +6,31 @@ import ChatSelectionPanel from "@/features/ChatSelectionPanel";
 import ChatPanel from "@/features/ChatPanel";
 
 const Dashboard = () => {
+    const [optionSelected, setOptionSelected] = useState("friends");
+
+    let mainDashboardContent = null;
+    switch (optionSelected) {
+        case "friends":
+        default:
+            mainDashboardContent = (
+                <>
+                <div className={styles["chat-selection-panel"]}>
+                    <ChatSelectionPanel />
+                </div>
+                <div className={styles["chat-panel"]}>
+                    <ChatPanel />
+                </div>
+                </>
+            );
+    }
+
     return (
         <div className={styles["wrapper"]}>
         <div className={styles["container"]}>
             <div className={styles["options-sidebar"]}>
                 <OptionsSidebar />
             </div>
-            <div className={styles["chat-selection-panel"]}>
-                <ChatSelectionPanel />
-            </div>
-            <div className={styles["chat-panel"]}>
-                <ChatPanel />
-            </div>
+            {mainDashboardContent}
         </div>
         </div>
     )
