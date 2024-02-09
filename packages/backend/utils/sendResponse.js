ce869712d8aef26e375e73438d31a61a8f979ef1
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:58 +0000

    Send stack trace if NODE_ENV=development & error is provided as argument

--- packages/backend/utils/sendResponse.js
+++ packages/backend/utils/sendResponse.js
@@ -1,9 +1,24 @@
-const sendResponse = (res, status, message, data) => {
-    return res.status(status).send({
-        status: status,
-        message: message,
-        data: data,
-    });
+const sendResponse = (
+    res,
+    status = 500,
+    message = "",
+    data = null,
+    err = null
+) => {
+    if (process.env.NODE_ENV === "development") {
+        return res.status(status).send({
+            status: status || err.status,
+            stack: err.stack || "",
+            message: message,
+            data: null,
+        });
+    } else {
+        return res.status(status).send({
+            status: status,
+            message: message,
+            data: data,
+        });
+    }
 };
 
 export default sendResponse;
