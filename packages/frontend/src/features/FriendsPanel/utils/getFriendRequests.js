32ac1f180873dc52dadc67298ae7560e8e601ea0
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:36:00 +0000

    Move FriendsPanel to its own feature component

---
+++ packages/frontend/src/features/FriendsPanel/utils/getFriendRequests.js
@@ -0,0 +1,42 @@
+import saveTokenFromResponseJSON from "@/utils/saveTokenFromResponseJSON.js";
+
+const getFriendRequests = async (abortController) => {
+    const data = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/user/friend-requests`,
+        {
+            signal: abortController ? abortController.signal : null,
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: localStorage.getItem(
+                    "odin-messaging-app-auth-token"
+                ),
+            },
+        }
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromResponseJSON(responseJSON);
+
+            if (responseJSON.status === 401) {
+                window.location.href = "/log-in";
+            }
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                friendRequests: responseJSON.data.friendRequests,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: 500,
+                message: "Requesting friend requests list failed",
+                friendRequests: [],
+            };
+        });
+    return data;
+};
+
+export default getFriendRequests;
