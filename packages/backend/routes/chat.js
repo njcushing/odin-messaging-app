f9799fb698f698b0fd4c31aef32ee3db76f0495f
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:59 +0000

    Write tests & functionality for /chat POST route & update router

--- packages/backend/routes/chat.js
+++ packages/backend/routes/chat.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import * as controller from "../controllers/chatController.js";
 
 router.get("/:chatId", controller.chatGet);
+router.post("/", controller.chatPost);
 
 export default router;
