ac4e793057c57569be4027827173ec01d481181f
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:59 +0000

    Change message response in validateUserCredentials function from object to string

--- packages/backend/utils/validateUserCredentials.js
+++ packages/backend/utils/validateUserCredentials.js
@@ -4,9 +4,9 @@ import User from "../models/user.js";
 
 const validateUserCredentials = async (username, password) => {
     const user = await User.findOne({ username: username });
-    if (!user) return [false, null, { message: "Incorrect username." }];
+    if (!user) return [false, null, "Incorrect username."];
     const match = await bcrypt.compare(password, user.password);
-    if (!match) return [false, null, { message: "Incorrect password." }];
+    if (!match) return [false, null, "Incorrect password."];
 
     // Ensure password returned is not the hashed version
     user.password = password;
