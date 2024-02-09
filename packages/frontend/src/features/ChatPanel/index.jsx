53fc26245f649de4fae3d9e7fd959c1cbfa09215
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:57 +0000

    Create boilerplate components for main parts of Dashboard page

---
+++ packages/frontend/src/features/ChatPanel/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./index.module.css";
+
+const ChatPanel = () => {
+    return (
+        <div className={styles["wrapper"]}>
+        <div className={styles["container"]}>
+        </div>
+        </div>
+    );
+};
+
+export default ChatPanel;
