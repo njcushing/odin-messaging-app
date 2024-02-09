0b83239a09cf4dc03f76a5e96832e7fd5fb50390
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:59 +0000

    Write functionality for finding users in database from client

--- packages/frontend/src/features/ChatSelectionPanel/components/AddFriendPanel/utils/findUser.js
+++ packages/frontend/src/features/ChatSelectionPanel/components/AddFriendPanel/utils/findUser.js
@@ -1,5 +1,36 @@
-const findUser = async () => {
-    return null;
+const findUser = async (username, abortController) => {
+    const data = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/user/${username}`,
+        {
+            signal: abortController.signal,
+            method: "GET",
+            mode: "cors",
+        }
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            let user = null;
+            if (
+                responseJSON.data !== null &&
+                typeof responseJSON.data === "object" &&
+                "user" in responseJSON.data
+            ) {
+                user = responseJSON.data.user;
+            }
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                user: user,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: 500,
+                message: "Requesting friends list failed",
+                user: null,
+            };
+        });
+    return data;
 };
 
 export default findUser;
