d66581a54b504ad768089b097fdd064d07377c3c
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:36:01 +0000

    Improve /chat routes' tests and coverage
    Also a few small fixes in some of the routes

--- packages/backend/controllers/__tests__/chat.test.js
+++ packages/backend/controllers/__tests__/chat.test.js
@@ -58,6 +58,28 @@ afterAll(() => mongoose.connection.close());
 
 describe("Route testing...", () => {
     describe("/chat/:chatId GET route...", () => {
+        test(`Should respond with status code 400 if the 'firstMessage' and/or
+         'lastMessage' query parameters fail their validation`, async () => {
+            mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
+            await request(app)
+                .get(`/${chats[0]._id}?firstMessage=a&lastMessage=1`)
+                .expect(400);
+            await request(app)
+                .get(`/${chats[0]._id}?firstMessage=1&lastMessage=a`)
+                .expect(400);
+            await request(app)
+                .get(`/${chats[0]._id}?firstMessage=0.1&lastMessage=1`)
+                .expect(400);
+            await request(app)
+                .get(`/${chats[0]._id}?firstMessage=1&lastMessage=0.1`)
+                .expect(400);
+            await request(app)
+                .get(`/${chats[0]._id}?firstMessage=-1&lastMessage=1`)
+                .expect(400);
+            await request(app)
+                .get(`/${chats[0]._id}?firstMessage=1&lastMessage=-1`)
+                .expect(400);
+        });
         test(`Should respond with status code 400 if the user '_id' value
          extracted from the token in the 'authorization' header is not a valid
          MongoDB ObjectId`, async () => {
@@ -188,6 +210,26 @@ describe("Route testing...", () => {
                 .set("Accept", "application/json")
                 .expect(404);
         });
+        test(`Should throw an error if the Chat schema .save() operation fails`, async () => {
+            mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
+            vi.spyOn(Chat.prototype, "save").mockImplementationOnce(
+                async () => {
+                    throw new Error("failed");
+                }
+            );
+            vi.spyOn(Chat, "findOne").mockReturnValueOnce(null);
+            mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
+            await request(app)
+                .post(`/`)
+                .send({ participants: [users[1]._id] })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
+        });
         test(`Should respond with status code 401 if, when there is only one
          participant, the currently logged-in user cannot be found in the
          database when trying to update its fields`, async () => {
@@ -245,8 +287,8 @@ describe("Route testing...", () => {
                     }
                 });
         });
-        test(`Should respond with an error if the Chat schema .save() operation
-         fails when trying to create one for more than two users`, async () => {
+        test(`Should throw an error if the Chat schema .save() operation fails
+         when trying to create one for more than two users`, async () => {
             vi.spyOn(Chat.prototype, "save").mockImplementationOnce(
                 async () => {
                     throw new Error("failed");
@@ -258,7 +300,11 @@ describe("Route testing...", () => {
                 .send({ participants: [users[4]._id, users[1]._id] })
                 .set("Content-Type", "application/json")
                 .set("Accept", "application/json")
-                .expect((res) => res.error !== false);
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
         });
         test(`Should respond with status code 404 if, when trying to create a
          chat for more than two users, at least one of the participants is not
@@ -374,8 +420,26 @@ describe("Route testing...", () => {
                 .set("Accept", "application/json")
                 .expect(403);
         });
-        test(`Should respond with an error if the Image schema .save() operation
-         fails`, async () => {
+        test(`Should throw an error if the Image schema .findByIdAndDelete()
+         operation fails`, async () => {
+            vi.spyOn(Image, "findByIdAndDelete").mockImplementationOnce(
+                async () => {
+                    throw new Error("failed");
+                }
+            );
+            mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
+            await request(app)
+                .put(`/${chats[0]._id}/image`)
+                .send({ image: [] })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
+        });
+        test(`Should throw an error if the Image schema .save() operation fails`, async () => {
             vi.spyOn(Image.prototype, "save").mockImplementationOnce(
                 async () => {
                     throw new Error("failed");
@@ -387,7 +451,11 @@ describe("Route testing...", () => {
                 .send({ image: [] })
                 .set("Content-Type", "application/json")
                 .set("Accept", "application/json")
-                .expect((res) => res.error !== false);
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
         });
         test(`Should respond with status code 404 if, when attempting to
          update the chat's fields, it cannot be found in the database`, async () => {
@@ -533,6 +601,47 @@ describe("Route testing...", () => {
                 .set("Accept", "application/json")
                 .expect(403);
         });
+        test(`Should throw an error if the Message schema .save() operation
+         fails`, async () => {
+            vi.spyOn(Message.prototype, "save").mockImplementationOnce(
+                async () => {
+                    throw new Error("failed");
+                }
+            );
+            mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
+            await request(app)
+                .post(`/${chats[0]._id}/message/text`)
+                .send({
+                    text: "Message",
+                    replyingTo: users[1]._id,
+                })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
+        });
+        test(`Should respond with status code 404 if, when trying to save the
+         new message in the chat's 'messages' array, the chat cannot be found
+         in the database`, async () => {
+            vi.spyOn(Chat, "findByIdAndUpdate").mockReturnValueOnce(null);
+            mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
+            await request(app)
+                .post(`/${chats[0]._id}/message/text`)
+                .send({
+                    text: "Message",
+                    replyingTo: users[1]._id,
+                })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
+        });
         test(`Should respond with status code 201 on successful request, with
          the _id of the new message and a valid token`, async () => {
             mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
@@ -669,8 +778,29 @@ describe("Route testing...", () => {
                 .set("Accept", "application/json")
                 .expect(403);
         });
-        test(`Should respond with an error if the Message schema .save()
-         operation fails`, async () => {
+        test(`Should throw an error if the Image schema .save() operation fails`, async () => {
+            vi.spyOn(Image.prototype, "save").mockImplementationOnce(
+                async () => {
+                    throw new Error("failed");
+                }
+            );
+            mockProtectedRouteJWT(users[0]._id, "Person1", "person1*");
+            await request(app)
+                .post(`/${chats[0]._id}/message/image`)
+                .send({
+                    image: [],
+                    replyingTo: users[1]._id,
+                })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
+        });
+        test(`Should throw an error if the Message schema .save() operation
+         fails`, async () => {
             vi.spyOn(Message.prototype, "save").mockImplementationOnce(
                 async () => {
                     throw new Error("failed");
@@ -685,7 +815,11 @@ describe("Route testing...", () => {
                 })
                 .set("Content-Type", "application/json")
                 .set("Accept", "application/json")
-                .expect((res) => res.error !== false);
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
         });
         test(`Should respond with status code 404 if, when trying to save the
          new message in the chat's 'messages' array, the chat cannot be found
@@ -854,8 +988,26 @@ describe("Route testing...", () => {
                 .set("Accept", "application/json")
                 .expect(403);
         });
-        test(`Should respond with an error if the Chat schema .save() operation
-         fails when trying to create a new chat from an existing
+        test(`Should throw an error if the User schema .updateMany() operation
+         fails when trying to add a new chat to those users' 'chats' array, if
+         the chat being added to is an 'individual'-type chat`, async () => {
+            vi.spyOn(User, "updateMany").mockImplementationOnce(async () => {
+                throw new Error("failed");
+            });
+            mockProtectedRouteJWT(users[1]._id, "Person1", "person1*");
+            await request(app)
+                .post(`/${chats[1]._id}/add-friends`)
+                .send({ participants: [users[0]._id] })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
+        });
+        test(`Should throw an error if the Chat schema .save() operation fails
+         when trying to create a new chat after attempting to add users to an
          'individual'-type chat`, async () => {
             vi.spyOn(Chat.prototype, "save").mockImplementationOnce(
                 async () => {
@@ -868,7 +1020,49 @@ describe("Route testing...", () => {
                 .send({ participants: [users[0]._id] })
                 .set("Content-Type", "application/json")
                 .set("Accept", "application/json")
-                .expect((res) => res.error !== false);
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
+        });
+        test(`Should throw an error if the User schema .updateMany() operation
+         fails when trying to add the chat's _id to the new users' 'chats'
+         array`, async () => {
+            vi.spyOn(User, "updateMany").mockImplementationOnce(async () => {
+                throw new Error("failed");
+            });
+            mockProtectedRouteJWT(users[5]._id, "Person6", "person6*");
+            await request(app)
+                .post(`/${chats[5]._id}/add-friends`)
+                .send({ participants: [users[3]._id, users[4]._id] })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
+        });
+        test(`Should throw an error if the Chat schema .findByIdAndUpdate()
+         operation fails when trying to add the new user's information to the
+         new chat's 'participants' array`, async () => {
+            vi.spyOn(Chat, "findByIdAndUpdate").mockImplementationOnce(
+                async () => {
+                    throw new Error("failed");
+                }
+            );
+            mockProtectedRouteJWT(users[1]._id, "Person1", "person1*");
+            await request(app)
+                .post(`/${chats[1]._id}/add-friends`)
+                .send({ participants: [users[0]._id] })
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .expect((res) => {
+                    if (!(res.error instanceof Error)) {
+                        throw new Error("Expected error to be thrown.");
+                    }
+                });
         });
         test(`Should respond with status code 201 on successful request, with
          a valid token`, async () => {
