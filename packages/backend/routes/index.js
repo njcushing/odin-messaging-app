0461a45b50a0ebc2c51be1a0e0b3f215eef01ff2
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:59 +0000

    Pass /log-in route through index route file

--- packages/backend/routes/index.js
+++ packages/backend/routes/index.js
@@ -2,10 +2,11 @@ import express from "express";
 const router = express.Router();
 
 import createaccount from "./createaccount.js";
+import login from "./login.js";
 import dashboard from "./dashboard.js";
 
 router.get("/", (req, res) => {
     res.send("");
 });
 
-export { router as index, createaccount, dashboard };
+export { router as index, createaccount, login, dashboard };
