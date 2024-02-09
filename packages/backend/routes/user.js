17014c4ceebc4718f13e7abfdd6d05d9b35015c7
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:36:00 +0000

    Write tests & functionality for /user/preferences/tagLine PUT route & update router

--- packages/backend/routes/user.js
+++ packages/backend/routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 import * as controller from "../controllers/userController.js";
 
 router.put("/preferences/displayName", controller.displayNamePut);
+router.put("/preferences/tagLine", controller.tagLinePut);
 router.get("/self/:username", controller.userSelf);
 router.get("/self", controller.getSelf);
 router.get("/friends/can-be-added/:username", controller.friendCanBeAdded);
