394783b780e26a1f370bfb28f2627519efd8640f
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:57 +0000

    Remove TooltipPropTypes from own file

--- packages/frontend/src/components/Tooltip/TooltipPropTypes.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from "prop-types";
-
-const TooltipPropTypes = {
-    text: PropTypes.string,
-    position: PropTypes.oneOf(["top", "right", "bottom", "left"]),
-    pixelBuffer: PropTypes.number,
-}
-
-export default TooltipPropTypes;
