09230c60d4ae9905938974ae991ec3faf7dafe36
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:59 +0000

    Write getFriend API function

---
+++ packages/frontend/src/features/ChatSelectionPanel/components/AddFriendPanel/utils/getFriend.js
@@ -0,0 +1,46 @@
+const getFriend = async (abortController) => {
+    const data = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/user/friend`,
+        {
+            signal: abortController.signal,
+            method: "GET",
+            mode: "cors",
+            headers: {
+                authorization: localStorage.getItem(
+                    "odin-messaging-app-auth-token"
+                ),
+            },
+        }
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            if (responseJSON.status === 401) {
+                window.location.href = "/log-in";
+            }
+            if (
+                responseJSON.data !== null &&
+                typeof responseJSON.data === "object" &&
+                "token" in responseJSON.data
+            ) {
+                localStorage.setItem(
+                    "odin-messaging-app-auth-token",
+                    responseJSON.data.token
+                );
+            }
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                friend: responseJSON.data.friend,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: 500,
+                message: "Finding friend failed",
+                friend: null,
+            };
+        });
+    return data;
+};
+
+export default getFriend;
