9d405a99e7cc5a255ea2b9ada88090fc94e4d69c
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:59 +0000

    Write functionality for logInAPI function

--- packages/frontend/src/pages/LogIn/utils/logInAPI.js
+++ packages/frontend/src/pages/LogIn/utils/logInAPI.js
@@ -1,8 +1,36 @@
-const logInAPI = async () => {
-    return {
-        status: 400,
-        message: "Log in attempt failed",
-    };
+const logInAPI = async (credentials) => {
+    const data = await fetch(`${import.meta.env.VITE_SERVER_DOMAIN}/log-in`, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+    })
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            if (
+                responseJSON.data !== null &&
+                typeof responseJSON === "object" &&
+                responseJSON.hasOwnProperty("token")
+            ) {
+                localStorage.setItem(
+                    "odin-messaging-app-auth-token",
+                    responseJSON.data.token
+                );
+            }
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: 500,
+                message: "Log-in attempt failed",
+            };
+        });
+    return data;
 };
 
 export default logInAPI;
