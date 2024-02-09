ed51b6648ccc810f58c14ae030b9f80ac524b23e
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:58 +0000

    Write boilerplate function for requesting friends list from API

---
+++ packages/frontend/src/features/ChatSelectionPanel/components/CreateChatPanel/utils/getFriendsList.js
@@ -0,0 +1,54 @@
+const getFriendsList = async () => {
+    return [
+        {
+            _id: "1",
+            name: "John Smith",
+            tagLine: "Example tagline",
+            status: "online",
+            imageSrc: "",
+            imageAlt: "",
+        },
+        {
+            _id: "2",
+            name: "Davey Jones",
+            tagLine: "Example tagline",
+            status: "away",
+            imageSrc: "",
+            imageAlt: "",
+        },
+        {
+            _id: "3",
+            name: "Elizabeth Swann",
+            tagLine: "Example tagline",
+            status: "busy",
+            imageSrc: "",
+            imageAlt: "",
+        },
+        {
+            _id: "4",
+            name: "William Turner",
+            tagLine: "Example tagline",
+            status: "offline",
+            imageSrc: "",
+            imageAlt: "",
+        },
+        {
+            _id: "5",
+            name: "Jack Sparrow",
+            tagLine: "Example tagline",
+            status: "offline",
+            imageSrc: "",
+            imageAlt: "",
+        },
+        {
+            _id: "6",
+            name: "Barbosa",
+            tagLine: "Example tagline",
+            status: "online",
+            imageSrc: "",
+            imageAlt: "",
+        },
+    ];
+};
+
+export default getFriendsList;
