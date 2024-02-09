46a912d112f565e6f234d597db3e00e75e0ca2e8
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-09 11:35:58 +0000

    Add a 'no chat' message to the ChatPanel component when returned chat is null

--- packages/frontend/src/features/ChatSelectionPanel/components/ChatPanel/index.jsx
+++ packages/frontend/src/features/ChatSelectionPanel/components/ChatPanel/index.jsx
@@ -11,7 +11,7 @@ import getChatFromAPI from "./utils/getChatFromAPI";
 import combineParticipantNames from "./utils/combineParticipantNames";
 
 const ChatPanel = () => {
-    const [chat, setChat] = useState({});
+    const [chat, setChat] = useState(null);
     const [messageSubmissionErrors, setMessageSubmissionErrors] = useState([]);
 
     useEffect(() => {
@@ -24,95 +24,105 @@ const ChatPanel = () => {
     return (
         <div className={styles["wrapper"]}>
         <div className={styles["container"]}>
-            <div
-                className={styles["top-bar"]}
-            >
-                <ProfileImage
-                    src={""}
-                    alt={""}
-                    sizePx={50}
-                />
-                {chat.chatName !== ""
-                ?   <h3
-                        className={styles["chat-name-custom"]}
-                        aria-label="chat-name"
-                    >{chat.chatName}</h3>
-                :   <h3
-                        className={styles["chat-name-participants"]}
-                        aria-label="chat-participants"
-                    >{combineParticipantNames(chat.participants, 3)}</h3>
-                }
-                <ul
-                    className={styles["chat-options"]}
-                    aria-label="chat-options"
+            {chat !== null
+            ?   <>
+                <div
+                    className={styles["top-bar"]}
                 >
-                    <OptionButton
-                        text="person_add"
-                        tooltipText="Add Person"
-                        tooltipPosition="bottom"
-                        widthPx={44}
-                        heightPx={44}
-                        fontSizePx={22}
-                        borderStyle="circular"
-                        onClickHandler={() => {}}
+                    <ProfileImage
+                        src={""}
+                        alt={""}
+                        sizePx={50}
                     />
-                    <OptionButton
-                        text="call"
-                        tooltipText="Call"
-                        tooltipPosition="bottom"
-                        widthPx={44}
-                        heightPx={44}
-                        fontSizePx={22}
-                        borderStyle="circular"
-                        onClickHandler={() => {}}
-                    />
-                    <OptionButton
-                        text="videocam"
-                        tooltipText="Video Call"
-                        tooltipPosition="bottom"
-                        widthPx={44}
-                        heightPx={44}
-                        fontSizePx={22}
-                        borderStyle="circular"
-                        onClickHandler={() => {}}
-                    />
-                </ul>
-            </div>
-            <div className={styles["messages-container"]}>
-                <ul
-                    className={styles["message-list"]}
-                    aria-label="chat-message-list"
+                    {chat.chatName !== ""
+                    ?   <h3
+                            className={styles["chat-name-custom"]}
+                            aria-label="chat-name"
+                        >{chat.chatName}</h3>
+                    :   <h3
+                            className={styles["chat-name-participants"]}
+                            aria-label="chat-participants"
+                        >{combineParticipantNames(chat.participants, 3)}</h3>
+                    }
+                    <ul
+                        className={styles["chat-options"]}
+                        aria-label="chat-options"
+                    >
+                        <OptionButton
+                            text="person_add"
+                            tooltipText="Add Person"
+                            tooltipPosition="bottom"
+                            widthPx={44}
+                            heightPx={44}
+                            fontSizePx={22}
+                            borderStyle="circular"
+                            onClickHandler={() => {}}
+                        />
+                        <OptionButton
+                            text="call"
+                            tooltipText="Call"
+                            tooltipPosition="bottom"
+                            widthPx={44}
+                            heightPx={44}
+                            fontSizePx={22}
+                            borderStyle="circular"
+                            onClickHandler={() => {}}
+                        />
+                        <OptionButton
+                            text="videocam"
+                            tooltipText="Video Call"
+                            tooltipPosition="bottom"
+                            widthPx={44}
+                            heightPx={44}
+                            fontSizePx={22}
+                            borderStyle="circular"
+                            onClickHandler={() => {}}
+                        />
+                    </ul>
+                </div>
+                <div className={styles["messages-container"]}>
+                    <ul
+                        className={styles["message-list"]}
+                        aria-label="chat-message-list"
+                    >
+                        {chat.messages && Array.isArray(chat.messages) && chat.messages.length > 0
+                        ?   chat.messages.map((message) => {
+                                return (
+                                    <li
+                                        aria-label="chat-message"
+                                        key={message._id}
+                                    ><Message
+                                        text={message.text}
+                                        name={message.author.name}
+                                        dateSent={message.dateSent}
+                                        imageSrc={message.author.imageSrc}
+                                        imageAlt={message.author.imageAlt}
+                                        position={message.author._id === "self" ? "right" : "left"}
+                                    /></li>
+                                )
+                            })
+                        :   <p
+                                className={styles["empty-chat-text"]}
+                                aria-label="empty-chat-text"
+                            >There are no messages in this chat yet!</p>}
+                    </ul>
+                </div>
+                <div
+                    className={styles["text-editor-container"]}
                 >
-                    {chat.messages && Array.isArray(chat.messages) && chat.messages.length > 0
-                    ?   chat.messages.map((message) => {
-                            return (
-                                <li
-                                    aria-label="chat-message"
-                                    key={message._id}
-                                ><Message
-                                    text={message.text}
-                                    name={message.author.name}
-                                    dateSent={message.dateSent}
-                                    imageSrc={message.author.imageSrc}
-                                    imageAlt={message.author.imageAlt}
-                                    position={message.author._id === "self" ? "right" : "left"}
-                                /></li>
-                            )
-                        })
-                    :   <p
-                            className={styles["empty-chat-text"]}
-                            aria-label="empty-chat-text"
-                        >There are no messages in this chat yet!</p>}
-                </ul>
-            </div>
-            <div
-                className={styles["text-editor-container"]}
-            >
-                <MessageBox
-                    submissionErrors={messageSubmissionErrors}
-                    onSubmitHandler={() => {}}
-                />
-            </div>
+                    <MessageBox
+                        submissionErrors={messageSubmissionErrors}
+                        onSubmitHandler={() => {}}
+                    />
+                </div>
+                </>
+            :   <div className={styles["no-chat-message-container"]}>
+                    <p
+                        className={styles["no-chat-message"]}
+                        aria-label="no-chat-here"
+                    >Create or open a chat to get chatting!</p>
+                </div>
+            }
         </div>
         </div>
     );
